49b1a36cf8908dae105678b4d8af9058d1e95863
Umer Zia <omarzia2514@gmail.com>
2021-03-27 15:18:45 +0500

    saturday...march27,2021...office

--- app/src/main/java/com/prototype/diabetescompanion/model/DoctorModel.kt
+++ app/src/main/java/com/prototype/diabetescompanion/model/DoctorModel.kt
@@ -2,11 +2,23 @@ package com.prototype.diabetescompanion.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "DoctorTable")
 class DoctorModel(
     @ColumnInfo(name = "name")
     var Name: String,
 
-    ) {
+    @ColumnInfo(name = "number")
+    var ContactNumber: String,
+
+    @ColumnInfo(name = "experience")
+    var Experience: Int,
+
+    @ColumnInfo(name = "hospital")
+    var Hospital: String,
+) {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var Id: Int? = null
 }
--- app/src/main/java/com/prototype/diabetescompanion/room/DiabetesDatabase.kt
+++ app/src/main/java/com/prototype/diabetescompanion/room/DiabetesDatabase.kt
@@ -4,16 +4,27 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.prototype.diabetescompanion.model.BGLReading
+import com.prototype.diabetescompanion.model.DoctorModel
 import com.prototype.diabetescompanion.model.PatientModel
 
-@Database(entities = [PatientModel::class, BGLReading::class], version = 1, exportSchema = false)
+@Database(entities = [DoctorModel::class, PatientModel::class, BGLReading::class],
+    version = 2,
+    exportSchema = false)
 abstract class DiabetesDatabase : RoomDatabase() {
     abstract fun diabetesDAO(): DiabetesDAO
 
     companion object {
         @Volatile
         private var INSTANCE: DiabetesDatabase? = null
+
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("CREATE TABLE `DoctorTable` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `number` TEXT NOT NULL, `experience` INTEGER NOT NULL, `hospital` TEXT NOT NULL)")
+            }
+        }
 
         fun getDatabase(context: Context): DiabetesDatabase {
             if (INSTANCE != null) return INSTANCE!!
@@ -22,6 +33,7 @@ abstract class DiabetesDatabase : RoomDatabase() {
                 INSTANCE =
                     Room.databaseBuilder(context, DiabetesDatabase::class.java, "DIABETES_DATABASE")
                         .fallbackToDestructiveMigration()
+                        .addMigrations(MIGRATION_1_2)
                         .build()
                 return INSTANCE!!
             }
