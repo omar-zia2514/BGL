854c7db08b170bff85d5446acfea28f7f8062b26
Umer Zia <omarzia2514@gmail.com>
2021-04-05 01:35:59 +0500

    sunday...apr04-21...home

---
+++ app/src/main/java/com/prototype/diabetescompanion/SigninActivity.kt
@@ -0,0 +1,11 @@
+package com.prototype.diabetescompanion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
